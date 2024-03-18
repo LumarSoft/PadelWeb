3c17585890f8411c6e63158243a295b89e7d837f
marcebenitez2 <marcebenitez0607@gmail.com>
2024-03-17 23:50:01 -0300

    feat: Desarrollo de navbar y dark/light mode

---
+++ src/shared/components/navbar/LinksNavbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { GiHamburgerMenu } from "react-icons/gi";
+import DarkModeSwitch from "./DarkModeSwitch";
+import Link from "next/link";
+import { useRouterHelper } from "@/shared/hooks/useRouterHelper";
+
+export const MobileDropdownMenu = () => {
+  const routerHelper = useRouterHelper();
+  const currentRoute = routerHelper.getCurrentRoute();
+
+  const routes = currentRoute
+    ? routerHelper.getRouteExcluding(currentRoute)
+    : [];
+  return (
+    <div className="md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <GiHamburgerMenu className="text-3xl" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {routes.map((route, index) => (
+            <DropdownMenuItem key={index}>
+              <Link href={route.path}>{route.name}</Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="">
+            <DarkModeSwitch />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export const Categories = () => {
+  const routerHelper = useRouterHelper();
+  const currentRoute = routerHelper.getCurrentRoute();
+
+  const routes = currentRoute
+    ? routerHelper.getRouteExcluding(currentRoute)
+    : [];
+
+  return (
+    <div className="md:flex gap-4 hidden">
+      {routes.map((route, index) => (
+        <Link key={index} href={route.path}>
+          {route.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
--- src/shared/components/navbar/NavbarComponent.tsx
+++ src/shared/components/navbar/NavbarComponent.tsx
@@ -1,75 +1,25 @@
-"use client";
+import React from "react";
+import Image from "next/image";
+import { LavalleLogo } from "@/shared/providers/ImageLocalProvider";
+import DarkModeSwitch from "./DarkModeSwitch";
+import { Categories, MobileDropdownMenu } from "./LinksNavbar";
+import TemplateFramer from "../framerMotion/FramerTemplate";
 
-import { Switch } from "@/components/ui/switch";
-import { FaShoppingCart } from "react-icons/fa";
-import { GiHamburgerMenu } from "react-icons/gi";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { useStoreMode } from "@/shared/context/StoreMode";
-import { motion } from "framer-motion";
-
-export const NavbarComponent = () => {
-  const { mode, setMode } = useStoreMode();
-
+const NavbarComponent = () => {
   return (
-    <motion.header
-      className="w-full h-20 flex items-center justify-between px-2 border-b md:px-10"
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
+    <TemplateFramer
+      style="w-full h-20 flex items-center justify-between px-4 border-b md:px-10 fixed"
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
     >
-      <div className="md:hidden">
-        <DropDown changeTheme={setMode} mode={mode} />
+      <Image src={LavalleLogo} alt="Lavalle Padel" width={60} height={60} />
+      <Categories />
+      <MobileDropdownMenu />
+      <div className="hidden md:block">
+        <DarkModeSwitch />
       </div>
-      {/* Cambiar aca */}
-      <span>Logo</span>
-      <div className="md:flex gap-4 hidden">
-        {[1, 2, 3, 4].map((x, i) => (
-          <span key={i}>Categoria</span>
-        ))}
-      </div>
-      <div className="flex gap-4 items-center">
-        <Switch
-          className="hidden md:block"
-          onCheckedChange={() => setMode()}
-          checked={mode === "dark" ? false : true}
-        />
-        <FaShoppingCart className="text-3xl" />
-      </div>
-    </motion.header>
+    </TemplateFramer>
   );
 };
 
-const DropDown = ({
-  changeTheme,
-  mode,
-}: {
-  changeTheme: () => void;
-  mode: string;
-}) => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <GiHamburgerMenu className="text-3xl" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Categorias</DropdownMenuLabel>
-        <DropdownMenuItem>Categoria1</DropdownMenuItem>
-        <DropdownMenuItem>Categoria2</DropdownMenuItem>
-        <DropdownMenuItem>Categoria3</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Button onClick={() => changeTheme()}>
-            {mode === "light" ? "Dark" : "Light"} Mode
-          </Button>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
+export default NavbarComponent;
